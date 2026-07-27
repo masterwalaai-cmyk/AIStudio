291ca1f063ddb094ebae61f7565f9379e6ea696c
masterwalaai-cmyk <masterwalaai@gmail.com>
2026-07-27 19:40:22 +0530

    Fix indentation

--- backend/app.py
+++ backend/app.py
@@ -75,20 +75,20 @@ def chat():
     """ + "\n" + prompt_context
 
         try:
-    response = genai_client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=contents
-    )
+            response = genai_client.models.generate_content(
+                model="gemini-2.5-flash",
+             contents=contents
+            )
 
-    return {
-        "reply": response.text
-    }
+        return {
+            "reply": response.text
+        }
 
-except Exception as e:
-    print(e)
-    return {
-        "reply": str(e)
-    }
+    except Exception as e:
+        print(e)
+        return {
+            "reply": str(e)
+        }
 
 
 app.run(host="0.0.0.0", port=5000)
